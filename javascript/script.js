8d7d31a3521b33b4da48ce5bb2e32dd107118569
nazimrasheed <mr.rasheed1991@gmail.com>
2021-03-21 21:27:30 +0000

    final touch to the js file

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var Capital = [
+var lowerCaseltr = [
   "A",
   "B",
   "C",
@@ -28,7 +28,7 @@ var Capital = [
   "Y",
   "Z",
 ];
-var small = [
+var upperCaseltr = [
   "a",
   "b",
   "c",
@@ -56,23 +56,89 @@ var small = [
   "y",
   "z",
 ];
-var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var specialC = ["@", "#", "$", "%", "<", "^", "<"];
 function generatePassword() {
-  var password = "";
-  var passwordlength = prompt(
-    "what is the length of your password.Make sure its between 8 and 128 character"
-  );
-  if (passwordlength >= 8 && passwordlength <= 128) {
-    //write confirm
-  } else {
-    alert("password length is ");
-  }
+  let isLowerCaseCharacters;
+  let isUpperCaseCharacters;
+  let isNumericCharacters;
+  let isSpecialCharacters;
+  let optionsArray = [];
+  let password = [];
+  const result = [];
 
-  return password;
+  let passwordLength = prompt(
+    "How many characters would you like in your password? Passwords must be at least 8 characters, but no more than 128 characters."
+  );
+
+  passwordLength = Number.parseInt(passwordLength);
+
+  //if password is shorter then 8 character an alert be shown that password length is shorter then limit
+  if (passwordLength < 8) {
+    alert("your password is shorter then 8 character");
+  }
+  //if password is more then 128 character an alert be shown that password length is longer then limit
+  if (passwordLength > 128) {
+    alert("your password is longer then 128 character");
+  }
+  // If the user presses cancel on the password length prompt dialogue box, then the following alert is displayed.
+  if (!passwordLength) {
+    alert("You must specify a password length");
+  }
+  if (passwordLength >= 8 && passwordLength <= 128) {
+    isLowerCaseCharacters = confirm(
+      "Would you like to include lower case characters in your password?"
+    );
+    isUpperCaseCharacters = confirm(
+      "Would you like to include upper case characters in your password?"
+    );
+    isNumericCharacters = confirm(
+      "Would you like to include numeric characters in your password?"
+    );
+    isSpecialCharacters = confirm(
+      "Would you like to include special characters in your password?"
+    );
+
+    if (isLowerCaseCharacters) {
+      optionsArray.push(...lowerCaseArray);
+    }
+    if (isUpperCaseCharacters) {
+      optionsArray.push(...upperCaseArray);
+    }
+    if (isNumericCharacters) {
+      optionsArray.push(...numbersArray);
+    }
+    if (isSpecialCharacters) {
+      optionsArray.push(...specialCharactersArray);
+    }
+    if (
+      !isLowerCaseCharacters &&
+      !isUpperCaseCharacters &&
+      !isNumericCharacters &&
+      !isSpecialCharacters
+    ) {
+      alert("You must select at least one character type");
+    }
+
+    console.log(optionsArray);
+
+    function randomPasswordGenerator() {
+      const randomIndex = Math.floor(Math.random() * optionsArray.length);
+      const randomChoice = optionsArray[randomIndex];
+
+      return randomChoice;
+    }
+
+    for (let i = 0; i < passwordLength; i++) {
+      let randomResult = randomPasswordGenerator();
+      result.push(randomResult);
+    }
+    console.log(result);
+
+    password = result.join("");
+
+    return password;
+  }
 }
 
-// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
