d5c84ce5f71bbfce50d2d14bab25ea854e58c1f5
nazimrasheed <mr.rasheed1991@gmail.com>
2021-05-15 09:57:33 +0100

    removed the error

--- READme.md
+++ READme.md
@@ -1,5 +1,11 @@
 # Password Generator
 
+i was asked to make the password generator and some of the code were given and i had to add javascript code to make it work according to the requirement.
+
+The task was hard for me at the beginning because javascript is kind of language which is totaly different to html and css.the concept is not the hard to understand but apply that to code is a big task.
+
+this is the last day to submit the assignment and i had to spend all my weekend just so i can submit by the end of today
+
 ![Screenshot (1)](./assets/Screenshot/screenshot_1.png)
 
 ![Screenshot (2)](./assets/Screenshot/screenshot_2.png)
---
+++ assets/js/script.js
@@ -0,0 +1,201 @@
+var generateBtn = document.querySelector("#generate");
+
+//  Global Variables
+const lowerCaseArray = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+const upperCaseArray = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+const numbersArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const specialCharacterArray = [
+  "!",
+  "?",
+  "£",
+  "$",
+  "%",
+  "*",
+  "&",
+  "@",
+  "#",
+  ":",
+  ";",
+  "~",
+  "{",
+  "}",
+];
+
+// Validate password length meets criteria
+const validatePasswordLength = (passwordLength) => {
+  return passwordLength >= 8 && passwordLength <= 128;
+};
+
+// Confirm user's character selection
+const confirmCharacterChoice = () => {
+  isLowerCase = confirm(
+    "Would you like your password to contain lowercase characters?"
+  );
+  isUpperCase = confirm(
+    "Would you like your password to contain uppercase characters?"
+  );
+
+  isNumber = confirm("Would you like your password to contain numbers?");
+
+  isSpecialCharacter = confirm(
+    "Would you like your password to contain special characters?"
+  );
+  return {
+    isLowerCase,
+    isUpperCase,
+    isNumber,
+    isSpecialCharacter,
+  };
+};
+
+// Validate that at least one character type has been chosen
+const validateCharacters = (characterChoices) => {
+  return (
+    characterChoices.isLowerCase ||
+    characterChoices.isUpperCase ||
+    characterChoices.isNumber ||
+    characterChoices.isSpecialCharacter
+  );
+};
+
+// Create combined array from selected character types
+const getOptionsArray = (characterChoices) => {
+  const optionsArray = [];
+  // Confirm which of the character types is truthy, and combine their characters into a new array
+  if (characterChoices.isLowerCase) {
+    optionsArray.push.apply(optionsArray, lowerCaseArray);
+  }
+  if (characterChoices.isUpperCase) {
+    optionsArray.push.apply(optionsArray, upperCaseArray);
+  }
+  if (characterChoices.isNumber) {
+    optionsArray.push.apply(optionsArray, numbersArray);
+  }
+  if (characterChoices.isSpecialCharacter) {
+    optionsArray.push.apply(optionsArray, specialCharacterArray);
+  }
+  return optionsArray;
+};
+
+// Function to randomize the characters in an array, takes in an array, returns a string
+const getRandomCharacters = (arr) => {
+  const randomCharacters = arr[Math.floor(Math.random() * arr.length)];
+  return randomCharacters;
+};
+
+// Set password array according to user selected length, takes in an array and the password length
+const getPasswordArray = (arr, passwordLength) => {
+  const passwordArray = [];
+  for (let i = 0; i < passwordLength; i++) {
+    const randomCharacters = getRandomCharacters(arr);
+    passwordArray.push(randomCharacters);
+  }
+  return passwordArray;
+};
+
+// Join an array into a string
+const joinPassword = (arr) => {
+  let password = arr.join("");
+  return password;
+};
+
+// Main App function
+const generatePassword = () => {
+  // Prompt for password length
+  let passwordLength = prompt(
+    "How many characters would you like your password to have?"
+  );
+  // convert passwordLength from string to number
+  passwordLength = parseInt(passwordLength);
+
+  // Validation of password length
+  if (validatePasswordLength(passwordLength)) {
+    const characterChoices = confirmCharacterChoice();
+
+    // Validation of character choices
+    if (validateCharacters(characterChoices)) {
+      const optionsArray = getOptionsArray(characterChoices);
+
+      // Set new password array
+      const passwordArray = getPasswordArray(optionsArray, passwordLength);
+      let password = joinPassword(passwordArray);
+
+      return password;
+    } else {
+      // If none of character types are truthy, show alert
+      alert("You must include at least one character type in your password!");
+    }
+
+    // if password doesn't meet criteria, show alert
+  } else {
+    alert(
+      "Password must have at least 8 characters, but no more than 128 characters."
+    );
+  }
+};
+
+// Write password to the #password input
+function writePassword() {
+  const password = generatePassword();
+
+  const passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
